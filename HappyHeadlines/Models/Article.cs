e4884616f425ee8fbcd0d25fbe9f44c69380137a
yonesswaidan <115068530+yonesswaidan@users.noreply.github.com>
2025-10-09 01:13:36 +0200

    Delete HappyHeadlines directory

--- HappyHeadlines/Models/Article.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HappyHeadlines.Models
-{
-    public class Article
-    {
-        public int Id { get; set; }
-        public string Headline { get; set; } = string.Empty;
-        public DateTime PublishDate { get; set; }
-    }
-}
